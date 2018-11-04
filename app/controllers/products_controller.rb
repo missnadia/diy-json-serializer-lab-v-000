23af221a2ae3f4c388686ae0d4a706cf5254a9da
Nadia Lee <miss.nadia.lee@gmail.com>
2018-11-04 22:10:22 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def index
     @products = Product.all
   end
@@ -20,6 +24,11 @@ class ProductsController < ApplicationController
   def create
     Product.create(product_params)
     redirect_to products_path
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
   end
 
   private
