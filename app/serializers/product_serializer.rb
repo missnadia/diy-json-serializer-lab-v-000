11c7b1dceb9457618752449a2b57758652039f9b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-11-04 21:55:05 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -3,8 +3,8 @@ class ProductSerializer
     serialized_product = '{'
     serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"price": "' + product.price + '", '
-    serialized_product += '"inventory": "' + product.inventory + '", '
+    serialized_product += '"price": "' + product.price.to_s + '", '
+    serialized_product += '"inventory": "' + product.inventory.to_s + '", '
     serialized_product += '"description": "' + product.description + '", '
     serialized_product += '}'
   end
